ea5e6abcbfe965bce2b461a1aad92208d2c9b1a5
kittycoffee <222090307@stu.nau.edu.cn>
2026-05-24 22:54:48 +0800

    让前端的 Vite 服务器来扮演 Nginx 的角色，给本地配置一个代理（Proxy）,同步云端服务器的代码

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,4 +15,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  // 下面是我们新加的 server 代理板块
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', // 指向你本地的 Spring Boot 后端
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '') // 剥离 /api 前缀，保持和云端 Nginx 逻辑一致
+      }
+    }
+  }
 })
